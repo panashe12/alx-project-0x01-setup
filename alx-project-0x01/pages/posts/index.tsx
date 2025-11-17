6bb9381424c7864cb62d1654daefbcf156fabd77
panashe12 <49037001+panashe12@users.noreply.github.com>
2025-11-17 23:14:38 +0200

    Refactor PostsPage to include PostModal and new post logic

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,16 +1,74 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
+import { PostProps }gi
 
-const PostsPage = () => {
+const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [newPost, setNewPost] = useState<PostData | null>(null);
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const handleAddPost = (post: PostData) => {
+    setNewPost({ ...post, id: posts.length + 1 });
+  };
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">Posts</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
+        </div>
 
-      <PostCard
-        title="First Post"
-        description="This is a sample post to show the PostCard component."
-      />
+        <div className="grid grid-cols-3 gap-2">
+          {posts.map(({ title, body, userId, id }: PostProps, key: number) => (
+            <PostCard
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+              key={key}
+            />
+          ))}
+
+          {newPost && (
+            <PostCard
+              title={newPost.title}
+              body={newPost.body}
+              userId={newPost.userId}
+              id={newPost.id}
+            />
+          )}
+        </div>
+      </main>
+
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
     </div>
   );
 };
 
-export default PostsPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
