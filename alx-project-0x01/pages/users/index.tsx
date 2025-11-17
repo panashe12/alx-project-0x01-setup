e8752e16f268d39c2ada43068a6f381fa3a14c7a
panashe12 <49037001+panashe12@users.noreply.github.com>
2025-11-17 23:32:18 +0200

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,57 @@
-const UsersPage = () => {
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
+
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [users, setUsers] = useState<UserProps[]>(posts);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUsers([...users, { ...newUser }]);
+  };
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">Users</h1>
-      <p className="text-gray-700">List of users will appear here...</p>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Users</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4 mt-4">
+          {users.map((user, idx) => (
+            <UserCard key={idx} {...user} />
+          ))}
+        </div>
+      </main>
+
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
     </div>
   );
 };
 
-export default UsersPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts:Map,
+    },
+  };
+}
+
+export default Users;
